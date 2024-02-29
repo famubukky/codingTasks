29394776622da511c812511c020e8144a01a106a
ALABA BUKOLA OGUNGBITE <108345028+famubukky@users.noreply.github.com>
2024-02-29 23:10:54 +0000

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,10 @@
+#This is an example to show how to input user's name and print his name
+name = input("My name is:    ")
+print(name)
+
+#This is an example to show how to input user's age and print his age
+age = input ("My age is:     ")
+print(age)
+
+#Line of code to dispaly hello world to the user
+print("Hello World")
